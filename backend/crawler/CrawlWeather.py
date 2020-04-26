b2e39d5dcdb1ea500a0f69e6b2e6423217c32fbd
Patrick <invader_zim@inode.at>
2020-04-26 09:15:51 +0200

    Bug fix in CrawlWeather for 'wetter.com' -> sun shine hours

--- backend/crawler/CrawlWeather.py
+++ backend/crawler/CrawlWeather.py
@@ -3,12 +3,13 @@ from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
 
 class CrawlWeather:
-    def __init__(self):
+    def __init__(self, update=True):
         self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/73.1'}
         self.eisbach_temperatures = []
         self.weather_forecast = []
 
-        self.getData()
+        if update:
+            self.getData()
 
     def getTemperature(self, date_start, date_end):
         url = "https://www.gkd.bayern.de/de/fluesse/wassertemperatur/isar/muenchen-himmelreichbruecke-16515005/monatswerte/tabelle?beginn=" + date_start.strftime("%d.%m.%Y") + "&ende=" + date_end.strftime("%d.%m.%Y")
@@ -29,7 +30,8 @@ class CrawlWeather:
 
     def getWeather(self):
 
-        url = 'https://www.wetter.com/wetter_aktuell/wettervorhersage/3_tagesvorhersage/deutschland/flughafen-muenchen-franz-josef-strauss-muc/DE0003033027.html'
+        #url = 'https://www.wetter.com/wetter_aktuell/wettervorhersage/3_tagesvorhersage/deutschland/flughafen-muenchen-franz-josef-strauss-muc/DE0003033027.html'
+        url  = 'https://www.wetter.com/wetter_aktuell/wettervorhersage/3_tagesvorhersage/deutschland/muenchen/DE0006515.html'
         r = requests.get(url)
 
         doc = BeautifulSoup(r.text, "html.parser")
@@ -46,9 +48,9 @@ class CrawlWeather:
                 data_min = element.select(".swg-col-temperature .swg-text-small")
                 data_rain= element.select(".swg-col-wv2")
 
-                param_min.append(float(data_min[0].text.strip().split('°')[0].split('/\u2009')[1]))
-                param_max.append(float(data_max[0].text.split('°')[0].strip()))
-                param_rain.append(float(data_rain[0].text.split('l/')[0].strip()))
+                param_min.append(float(data_min[0].text.strip().split('°')[0].split('/\u2009')[1].replace(',', '.')))
+                param_max.append(float(data_max[0].text.split('°')[0].strip().replace(',', '.')))
+                param_rain.append(float(data_rain[0].text.split('l/')[0].strip().replace(',', '.')))
 
             count += 1
 
@@ -57,7 +59,11 @@ class CrawlWeather:
         liste_son = doc.select(".spaces-weather-grid .swg-row-info")
         for element in liste_son:
             if "Sonnenstunden" in element.text:
-                param_sun.append(float(element.text.split("Sonnenstunden")[0].split("Freuen Sie sich auf bis zu")[1]))
+                # Check which phrase is forecasting the sun hours and change to right number format
+                if ("Heute gibt es bis zu" in element.text.split("Sonnenstunden")[0]):
+                    param_sun.append(float(element.text.split("Sonnenstunden")[0].split("Heute gibt es bis zu")[1].replace(',','.')))
+                else:
+                    param_sun.append(float(element.text.split("Sonnenstunden")[0].split("Freuen Sie sich auf bis zu")[1].replace(',', '.')))
 
         forecast_data = []
 
