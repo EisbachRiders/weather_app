d184b20e133b23a0ed05657f301733ffcf46418b
Alyssa <ca.butler10@gmail.com>
2020-05-31 14:10:53 +0200

    update requirements

--- backend/api.py
+++ backend/api.py
@@ -1,18 +1,10 @@
 from flask import Flask, jsonify, request, abort, make_response
 import json
-from main import main
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    with open('./data/forecast.json') as f:
-        data = json.load(f)
-    return jsonify(data)
-
-@app.route("/forecast")
-def forecast():
-    main()
     with open('./data/forecast.json') as f:
         data = json.load(f)
     return jsonify(data)
